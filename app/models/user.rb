6a41769d7dbfb1d91db7c4887d6f946555d103e1
tochisuke221 <tochisuke221@gmail.com>
2021-04-02 18:30:05 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :c_last_name,presence: true
+  validates :c_first_name,presence: true
+  validates :j_last_name,presence: true
+  validates :j_first_name,presence: true
+  validates :birthday,presence: true
 end
