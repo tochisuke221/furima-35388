8d282d642948e97af65043493b18846a78c86211
tochisuke221 <tochisuke221@gmail.com>
2021-04-03 21:48:37 +0900

    バリデーションとテストコードの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :c_last_name, presence: true
-  validates :c_first_name, presence: true
-  validates :j_last_name, presence: true
-  validates :j_first_name, presence: true
-  validates :birthday, presence: true
-  validates :nickname, presence: true
+  validates :password,format:{with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+
+  with_options presence:true do
+    validates :c_last_name,format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    validates :c_first_name,format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    validates :j_last_name,format: { with: /\A[ァ-ヶ]+\z/ }
+    validates :j_first_name,format: { with: /\A[ァ-ヶ]+\z/ }
+    validates :birthday
+    validates :nickname
+  end
+
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :user do
     nickname { Faker::Name.initials(number: 2) }
     email                   { Faker::Internet.free_email }
-    password                { Faker::Internet.password(min_length: 6) }
+    password                { "111qwe" }
     password_confirmation   { password }
-    c_last_name             { Faker::Name.initials(number: 2) }
-    c_first_name            { Faker::Name.initials(number: 2) }
-    j_last_name             { Faker::Name.initials(number: 2) }
-    j_first_name            { Faker::Name.initials(number: 2) }
+    c_last_name             { Gimei.last.kanji }
+    c_first_name            { Gimei.first.kanji }
+    j_last_name             { Gimei.last.katakana }
+    j_first_name            { Gimei.last.katakana }
     birthday { Faker::Date.between(from: '1930-01-01', to: '2016-12-31') }
   end
 end
