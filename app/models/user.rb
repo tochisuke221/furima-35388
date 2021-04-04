d3be2ff1f43d4f68a5ad2d76907622f04e63cc42
tochisuke221 <tochisuke221@gmail.com>
2021-04-04 15:51:00 +0900

    ユーザ機能のミス修正（こっそり）

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,14 +16,14 @@ class User < ApplicationRecord
   end
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
-    validates :last_name
-    validates :first_name
+    validates :c_last_name
+    validates :c_first_name
   end
 
  
   with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }do
-    validates :last_name_kana
-    validates :first_name_kana
+    validates :j_last_name
+    validates :j_first_name
   end
 
 end
