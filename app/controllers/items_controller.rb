83cec9c67831325560e4533973fc5ff24d5c86c2
tochisuke221 <tochisuke221@gmail.com>
2021-04-04 17:17:33 +0900

    もろもろ機能追加2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,29 +1,35 @@
 class ItemsController < ApplicationController
-  def index
-  end
+
+  before_action :move_to_index,only:[:new,:create] #のちのち、except[:show,:index]の変更する
+
+
 
   def new
     @item=Item.new
   end
 
   def create
+    @item=Item.new(item_params)
+
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
-  def edit
-  end
-
-  def update
-  end
-
-  def show
-  end
+  
 
 
   private
 
-  def 
+  def item_params
+    params.require(:item).permit(:name,:description,:category_id,:status_id,:fee_id,  :prefecture_id,:term_id,:price,:image).merge(user_id: current_user.id)
+  end
 
-end
-def item_params
-  params.require(:item).permit(:, :image).merge(user_id: current_user.id)
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
 end
--- app/models/prefecture.rb
+++ app/models/prefecture.rb
@@ -2,7 +2,7 @@ class Prefecture < ActiveHash::Base
 
 
 self.data = [
-  {id: 1, name:  '北海道'},
+  {id: 1, name:  '---'},
   {id: 2, name:  '青森県'},
   {id: 3, name: '岩手県'},
   {id: 4, name: '宮城県'},
@@ -48,7 +48,8 @@ self.data = [
   {id: 44, name: '大分県'},
   {id: 45, name: '宮崎県'},
   {id: 46, name: '鹿児島県'},
-  {id: 47, name: '沖縄県'}
+  {id: 47, name: '沖縄県'},
+  {id: 48, name: '北海道'}
   ]
 
 
