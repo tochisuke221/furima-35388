4fb6fbaab700a4e32e375eb0bd10bf8efd2d867e
tochisuke221 <tochisuke221@gmail.com>
2021-04-04 12:47:25 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,19 @@ class User < ApplicationRecord
   validates :password,format:{with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
 
   with_options presence:true do
-    validates :c_last_name,format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-    validates :c_first_name,format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-    validates :j_last_name,format: { with: /\A[ァ-ヶ]+\z/ }
-    validates :j_first_name,format: { with: /\A[ァ-ヶ]+\z/ }
     validates :birthday
     validates :nickname
+  end
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+    validates :last_name
+    validates :first_name
+  end
+
+ 
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }do
+    validates :last_name_kana
+    validates :first_name_kana
   end
 
 end
